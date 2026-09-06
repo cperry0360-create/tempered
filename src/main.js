ab25a5fba79f28d1a390358d6143e632aa176756
cperry0360-create <cperry0360@gmail.com>
2026-09-06 19:19:04 -0400

    Install battle FX through the app entry point

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { registerServiceWorker } from './pwa/register.js'
 import { bootstrap } from './app/bootstrap.js'
 import { installSessionGuard } from './ui/session-guard.js'
 import { errorState } from './ui/states.js'
+import { installBattleFx } from './ui/battle-fx-runtime.js'
 
 const WATER_FIXED_PRESETS = [8, 12]
 
@@ -160,6 +161,7 @@ function installWaterQuickPresets() {
 
 registerServiceWorker()
 installWaterQuickPresets()
+installBattleFx()
 
 bootstrap()
   .then((context) => { installSessionGuard(context) })
